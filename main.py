f111abd948c57b8dc705f8dde35fee228b14f378
cdutten <cdutten@gmail.com>
2024-07-18 19:41:07 +0200

    Modify the API to store the Goals on the DB instead of a txt file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import hug
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from models.goals import Goals
+from datetime import date
 
 @hug.response_middleware()
 def process_data(request, response, resource):
@@ -8,10 +11,20 @@ def process_data(request, response, resource):
     response.set_header('Access-Control-Allow-Headers', 'Content-Type')
 
 
+def connect_to_db():
+    engine = create_engine('sqlite:///411-tracker-api.sqlite')
+    conn = engine.connect()
+    return Session(conn)
+
 @hug.post()
-def goal(goal: hug.types.text, timeframe: hug.types.text, date: hug.types.text):
-    with open('goals.txt', 'a') as file:
-        file.write(goal + "," + timeframe + "," + date + "\n")
+def goal(name: hug.types.text, timeframe: hug.types.text):
+    session = connect_to_db()
+
+    goal = Goals(name=name, timeframe=date.fromisoformat(timeframe))
+    session.add(goal)
+    session.flush()
+    session.commit()
+
     return "Goal created"
 
 
---
+++ models/goals.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String
+from sqlalchemy import DateTime
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from models.base import Base
+
+class Goals(Base):
+    __tablename__ = "goals"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255))
+    timeframe: Mapped[DateTime] = mapped_column(DateTime())
